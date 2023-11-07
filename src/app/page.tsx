7915b06af9f994c49a2ce72524a22084e0ce37b1
Florian Douay <flooooo85@gmail.com>
2023-11-07 01:06:29 +0100

    added scroll to top button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import './globals.css'
 import Header from './_components/Header'
 import Footer from './_components/Footer'
 import AnimationCanvas from './_components/AnimationCanvas'
+import ScrollToTop from './_components/ScrollToTop'
+/* Page Components Imports */
 import FirstSection from './_pageComponents/FirstSection'
 import SecondSection from './_pageComponents/SecondSection'
 import ThirdSection from './_pageComponents/ThirdSection'
-/* Page Components Imports */
 
 
 
@@ -19,6 +20,7 @@ export default function Home() {
                 <FirstSection />
                 <SecondSection />
                 <ThirdSection />
+                <ScrollToTop />
             </main>
             <Footer />
         </>
